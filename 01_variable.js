022e7114974bb298562ea3108ce0da3e9103e035
Dipali3014 <dipaliwaghmare1790@gmail.com>
2023-09-20 14:04:32 +0530

    variable assign page
    variable assign page

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+var firstName; // variable declaration
+firstName = "Dipali";  // Variable initialization
+console.log(firstName);
+firstName = "Mrs. Dipali"; // Variable update
+console.log(firstName);
+
+
+var lastName = "Waghmare"; // variable declaration and initialization
+console.log(lastName);
+
+var height; // Variable Declaration
+console.log(height);
+height = "6"; // initialization
+console.log(height);
+height = "6.2feet";// update
+console.log(height);
