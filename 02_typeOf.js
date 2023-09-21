d1aaa1b2d4670392df8f1ee900e8e39bb75671c5
Dipali3014 <dipaliwaghmare1790@gmail.com>
2023-09-21 16:45:01 +0530

    data type of assignment
    data type of assignment

---
+++ 02_typeOf.js
@@ -0,0 +1,31 @@
+ console.log("#############################02_TypesOfJS Assignment:-2#################################################");
+ console.log("#########################Step 1##########################################");
+
+ var bankName
+ console.log( "Bank Name:",bankName, "data type ===>",typeof bankName);
+ 
+ var bankName= "SBI Bank";
+ console.log( "Bank Name :",bankName,"data type ==>",typeof bankName);
+
+ console.log("#######################Step 2########################################");
+
+ var marks='90%'; 
+ console.log("marks:",marks,"data type ==>", typeof marks);
+
+ console.log("########################## Step 3#############################################");
+ var isWorking 
+ console.log("isWorking :",isWorking,"data type ==>",typeof isWorking);
+
+ var isWorking = true;
+ console.log("isWorking :",isWorking,"data type ==>",typeof isWorking);
+ console.log("###########################Step 4####################################################");
+ console.log("------------------------------A--------------------------------------------------------");
+
+  var totalCount ="Hundred and Seven";
+  console.log(" totalCount:", totalCount,"data type ==>", typeof totalCount);
+   console.log("----------------------------B---------------------------------------------------");
+
+   var totalCount = 107;
+   console.log("totalCount :",totalCount,"data type ===>",typeof totalCount);
+
+
---
+++ 02_variableBasicAssign.js
@@ -0,0 +1,35 @@
+console.log("=======================01_variableBasicAssignment No:-2=====================================================");
+console.log("============================Steps of var=======================================");
+var countryName= "Country:-India";
+console.log("countryName ==>",countryName, "data type ===>",typeof countryName);
+
+var stateName =" State:-Maharashtra";
+console.log("stateName ==>",stateName,"data type ===>",typeof stateName);
+
+var Dist = " Dist:-Satara";
+console.log("Dist ==>",Dist,"data type ==>",typeof Dist);
+
+console.log("===============================Steps of Number=====================================");
+
+var myKids=  2
+
+console.log("myKids==>",myKids, " data type ===>",typeof myKids);
+
+var mySonBirthday = 30_10_2014
+console.log("mySonBirthday==>" ,mySonBirthday, "data type ===>", typeof mySonBirthday);
+
+var myDaughterBirthDay = 5_10_20
+console.log("myDaughterBirthDay==>",myDaughterBirthDay,"data type ==>",typeof myDaughterBirthDay);
+
+console.log("=====================================Steps of Boollean==================================");
+
+var isSunny =  false
+console.log("IsSunny==>",isSunny,"data type ===>", typeof isSunny);
+
+ var isHappy =  true
+ console.log("isHappy==>",isHappy, "data type ===>", typeof isHappy);
+
+ var isClass =  true
+ console.log("isClass==>",isClass, "data type ===>",typeof isClass);
+console.log("==================================End first assignment================================");
+
