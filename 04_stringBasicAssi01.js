26e19f9935cc972390a7716696dd8c9c67ffc852
Dipali3014 <dipaliwaghmare1790@gmail.com>
2023-09-25 18:13:16 +0530

    string basic assignment page
    string basic assignment page

---
+++ 04_stringBasicAssi01.js
@@ -0,0 +1,38 @@
+console.log(
+  "--------------------  Assignment 2  ----------------------------------------------"
+);
+console.log(
+  "-------------------- String Basic -----------------------------------------------"
+);
+
+function stringBasics() {
+  console.log("My Dream Company : Wipro IT Corporation");
+}
+stringBasics();
+console.log(
+  " --------------------- MY Hobbies ------------------------------------------------"
+);
+
+function myHobbies(hobby1, hobby2, hobby3) {
+  var myHobbies;
+  console.log(
+    "hobby1===> Traveling,",
+    "hobby2===> Cooking,",
+    "hobby3===> Watching T.V. "
+  );
+}
+myHobbies();
+
+var hobby1 = "hobby1===> Travelling";
+console.log(
+  "------------- Sum of total numbers in characters ---------------------------------"
+);
+var totalLength = hobby1.length;
+console.log("1.1]Length of the string is: ", totalLength);
+var hobby2 = "hobby2===> Cooking";
+var totalLength = hobby2.length;
+console.log("1.2]Length of the string is: ", totalLength);
+var hobby3 = "hobby3===> Watching T.V";
+var totalLength = hobby2.length;
+console.log("1.3]Length of the string is: ", totalLength);
+console.log("---------------------- End ------------------------------------------------");
