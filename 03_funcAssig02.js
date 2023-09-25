286e02c827f7ca67f7e509585663f417696772c1
Dipali3014 <dipaliwaghmare1790@gmail.com>
2023-09-25 17:35:04 +0530

    bank details page
    bank details page

---
+++ 03_funcAssig02.js
@@ -0,0 +1,9 @@
+function bankDetails(arg1,arg2,arg3,arg4){
+    console.log("----------------------Welcome To All Bank Details-------------------------------------------");
+    console.log("Bank Name: CITI Bank,","Account No:3456782345,","City Name: Pune,","PinCode:431202");
+    console.log("-----------------------------------------------------------------------------------");
+    console.log("Bank Name: Axis Bank,", "Account No:7856782345,","City Name: Mumbai,","PinCode:441202");
+    console.log("--------------------------------------------------------------------------------------");
+    console.log("Bank Name: HDFC Bank," ,"Account No:8956782345,","City Name: Pune,","PinCode:631202","open");
+}
+bankDetails();
