cb6dcf4382136c40decfcd502a18da52499cb33d
Dipali3014 <dipaliwaghmare1790@gmail.com>
2023-09-26 12:39:00 +0530

    string Template Assignment
    string Template Assignment

---
+++ 03_stringTemplateAssig.js
@@ -0,0 +1,37 @@
+console.log(
+    "--------------------  Assignment 1  -------------------------------------"
+  );
+  console.log(
+    "-------------------- String Template -------------------------------------"
+  );
+  
+  function stringBasics() {
+    console.log(`My Dream Company : "Wipro" IT Corporation`);
+  }
+  stringBasics();
+  console.log(
+    " --------------------- MY Hobbies -----------------------------------------"
+  );
+  
+  function myHobbies(hobby1, hobby2, hobby3) {
+    var myHobbies;
+    console.log(
+      `hobby1===> "Traveling", "Cooking","Stitching".`
+    );
+  }
+  myHobbies();
+  
+  var hobby1 = "hobby1===> Travelling";
+  console.log(
+    "------------- Sum of total numbers in characters ----------------------------"
+  );
+  var totalLength = hobby1.length;
+  console.log(`1.1]Length of the string is: "${totalLength}"`);
+  var hobby2 = "hobby2===> Cooking";
+  var totalLength = hobby2.length;
+  console.log(`1.2]Length of the string is: "${totalLength}"`);
+  var hobby3 = "hobby3===> Watching T.V";
+  var totalLength = hobby2.length;
+  console.log(`1.3]Length of the string is: "${totalLength}"`);
+  console.log("---------------------- End ----------------------------------------");
+  
