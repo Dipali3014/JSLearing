ef86fb454d507488991200f3166cd37c979d3ea5
Dipali3014 <dipaliwaghmare1790@gmail.com>
2023-09-27 19:56:28 +0530

    operator b ass
    operator b ass

---
+++ 05_operatorB.js
@@ -0,0 +1,69 @@
+console.log("------------------------ Assignment No:- 2   -----------------------------------------------------   ");
+function greaterNumbers(num1, num2) {}
+greaterNumbers();
+var num1 = 10;
+var num2 = -10;
+
+var result = num1 > num2;
+console.log(`Result of greater number is: ${result} ${num1}`);
+
+var abc1 = 800;
+var abc2 = 899;
+var result = abc1 < abc2;
+console.log(`Result of greater numbers is: ${result} ${abc2}`);
+console.log(
+  "======================================================================================"
+);
+
+function isEvenOrOddNum(str) {}
+isEvenOrOddNum();
+var num1 = 13;
+var result = num1 % 2 == 0 ? "EVEN" : "ODD";
+console.log(`Given number is ${num1} ==> ${result}`);
+
+var num2 = 44;
+var result = num2 % 2 == 0 ? "EVEN" : "ODD";
+console.log(`Given number is ${num2} ==> ${result}`);
+
+var num3 = 0;
+var result = num3 % 2 == 0 ? "EVEN" : "ODD";
+console.log(`Given number is ${num3} ==> ${result}`);
+
+var num4 = 101;
+var result = num4 % 2 == 0 ? "EVEN" : "ODD";
+console.log(`Given number is ${num4} ==> ${result}`);
+console.log(
+  "=========================================================================================="
+);
+
+function wordLength() {}
+wordLength();
+var str1 = "JavaScript";
+var totalLength = str1.length;
+var result = totalLength % 2 == 0 ? "Even" : "ODD";
+console.log(
+  ` Length of "JavaScript"word is:"${totalLength}" than Javascript word is ==> ${result}`
+);
+
+console.log(
+  "==========================================================================================="
+);
+
+var str2 = "developer";
+var totalLength = str2.length;
+var result = totalLength % 2 == 0 ? "Even" : "ODD";
+console.log(
+  ` Length of developer words is:"${totalLength}" than developer word is ==> ${result}`
+);
+
+console.log(
+  "============================================================================================"
+);
+
+var str3 = "Google";
+var totalLength = str3.length;
+var result = totalLength%2 == 0 ? "Even" : "ODD";
+
+console.log(` Length of Google word is:"${totalLength}" than Google word is  ==> ${result}`);
+ console.log("=============================================================================================="); 
+
