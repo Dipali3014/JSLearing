9752f9eac9d5915755b1c6cbe76b8439e84fdce6
Dipali3014 <dipaliwaghmare1790@gmail.com>
2023-09-27 19:49:20 +0530

    operator 1 assignment page
    operator 1 assignment page

---
+++ 05_operatorA.js
@@ -0,0 +1,68 @@
+console.log("====================================== Assignment No:-  ====================================================");
+function squareOfWordLength(Str1) {
+    console.log("==========================================================================================");
+    console.log("A1.------------------------------  JavaScript   -----------------------------------------------------------------");
+      var str1 = "JavaScript"
+    var lengthOfStr = Str1.length ;
+    var result = lengthOfStr;
+    console.log(`"Length of "JavaScript"  word is":   ${result}`);   
+}
+squareOfWordLength("JavaScript");
+function square(num){
+    var result = num * num; 
+    return result;
+ }
+ var squareNum = square(10);
+ console.log(`"Square of "JavaScript" word is":   ${squareNum} `);
+console.log("===============================================================================================");
+ console.log("2.-------------------------------   Google Chrome -----------------------------------------------------------------");
+ 
+ var str2 = "Google Chrome"
+ var lengthOfStr2 = str2.length;
+ var result = lengthOfStr2;
+ console.log(`"Length of "Google Chrome"  word is":   ${result}`);
+ var squareNum = square(13);
+ console.log(`"Square of "Google Chrome" word is":   ${squareNum}`);
+ console.log("==============================================================================================");
+ console.log("3.--------------------------------- Developer Smart -------------------------------------------------------------");
+               
+ var str3 = "Developer Smart "
+ var lengthOfStr3 = str3.length;
+ var result = lengthOfStr3;
+ console.log(`"Length of "Developer Smart" word is":   ${result}`);
+  var squareNum= square(16);
+  console.log(`"Square of "Developer Smart"  word is":  ${squareNum}`);
+  console.log("=============================================================================================");
+  console.log("B2.--------------------------------------------------------------------------------------------");
+
+  function aDeveloper() {
+    var developer = "I am Angular developer";
+    var totalLength = developer.length;
+    
+    console.log(`"Total length "I am Angular developer" word is:"   ${totalLength}`);
+    
+    
+    
+  }
+  aDeveloper();
+  console.log("1.-------------------------------------------  A  --------------------------------------------------------");
+var str ="I am Angular developer" ;
+  var words = str.split(" ");
+  console.log(words);
+  console.log("=============================================================================================");
+  console.log("2.--------------------------------------------- B  ---------------------------------------------------------------");
+
+  console.log('Total words is: ', words.length);
+  console.log("==============================================================================================");
+  console.log("3.---------------------------------------------  C  ---------------------------------------------------------------");
+   
+  var num1 = 22;
+  var num2 = 4;
+  var result = num1 / num2;
+  console.log(`" Output of length and word Division is:"${result}`);
+  console.log("============================================================================================");
+  console.log("4.----------------------------------------------   D ---------------------------------------------------------------");
+  var result = num1 * num2 ;
+  console.log(`" Multiplication of word length and words is:"${result}`);
+
+  console.log("----------------------     End  --------------------------------------------------------------");
