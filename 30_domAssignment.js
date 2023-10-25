5478bbc116c42da7bd56f81d8a84e655c296ecb0
Dipali3014 <dipaliwaghmare1790@gmail.com>
2023-10-25 23:30:18 +0530

    dom Assignment js and html
    dom Assignment js and html

---
+++ 30_domAssignment.js
@@ -0,0 +1,9 @@
+const h1Element = document.querySelector('h1');
+console.log(h1Element);
+
+const secondElement = document.getElementById('Advantages');
+console.log(secondElement.id);
+
+
+const advElement = document.querySelector('#Advantages')
+console.log(advElement.innerHTML);
